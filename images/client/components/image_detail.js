270fcf4c79c79ed0021dcc25be898d97eba6a85b
Alberto Muñoz <alberto.mqo@gmail.com>
2016-05-31 16:23:56 +0200

    Create image score to show the up and down votes for each image, and add it to image detail component.

--- images/client/components/image_detail.js
+++ images/client/components/image_detail.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
+import ImageScore from './image_score';
 
 class ImageDetail extends Component {
 
   render() {
+    let image = this.props.image;
     return (
       <li class="media list-group-item">
           <div className="media-left">
-            <img src={ this.props.image.link } />
+            <img src={ image.link } />
           </div>
           <div className="media-body">
             <h4 className="media-heading">
-              { this.props.image.title }
+              { image.title }
             </h4>
-            <p>{ this.props.image.description }</p>
+            <p>{ image.description }</p>
+            <ImageScore ups={ image.ups } downs={ image.downs } />
           </div>
       </li>
     );
