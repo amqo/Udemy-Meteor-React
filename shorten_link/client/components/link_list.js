e4b3d6f47adefbc85435cf0f3a2ac579af544c76
Alberto Muñoz <alberto.mqo@gmail.com>
2016-06-02 15:16:33 +0200

    Render rows with links in LinkList component

--- shorten_link/client/components/link_list.js
+++ shorten_link/client/components/link_list.js
@@ -4,10 +4,35 @@ import { Links } from '../../imports/collections/links';
 
 class LinkList extends Component {
 
+  renderRows() {
+    return this.props.links.map(link => {
+      const { url, clicks, token } = link;
+      const shortLink = `http://localhost:3000/${token}`;
+
+      return (
+        <tr key={ token }>
+          <td>{ url }</td>
+          <td>
+            <a href={ shortLink }>{ shortLink }</a>
+          </td>
+          <td>{ clicks }</td>
+        </tr>
+      );
+    });
+  }
+
   render() {
-    let links = this.props.links;
     return (
-      <div>List of Links</div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>URL</th>
+            <th>Address</th>
+            <th>Clicks</th>
+          </tr>
+        </thead>
+        <tbody>{ this.renderRows() }</tbody>
+      </table>
     );
   }
 }
