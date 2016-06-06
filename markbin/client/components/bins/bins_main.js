1170405b249e82b18f35643c2853b52ce754d032
Alberto Muñoz <alberto.mqo@gmail.com>
2016-06-06 14:09:35 +0200

    Show markdown code translated into actual HTML in a now component BinsViewer

--- markbin/client/components/bins/bins_main.js
+++ markbin/client/components/bins/bins_main.js
@@ -3,12 +3,16 @@ import { createContainer } from 'meteor/react-meteor-data';
 
 import { Bins } from '../../../imports/collections/bins';
 import BinsEditor from './bins_editor';
+import BinsViewer from './bins_viewer';
 
 class BinsMain extends Component {
 
   render() {
     return (
-      <BinsEditor bin={ this.props.bin }/>
+      <div>
+        <BinsEditor bin={ this.props.bin }/>
+        <BinsViewer bin={ this.props.bin }/>
+      </div>
     );
   }
 }
@@ -16,6 +20,6 @@ class BinsMain extends Component {
 export default createContainer((props) => {
   Meteor.subscribe('bins');
   const { binId } = props.params;
-  
+
   return { bin: Bins.findOne(binId) };
 }, BinsMain);
